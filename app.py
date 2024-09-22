d6ef62266e4d4ce08e91260725096de79c11b384
Anez <31077044+alasal17@users.noreply.github.com>
2024-09-22 06:08:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 api.init_app(app)
 
 port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
